f5fb17bbdca2d7d452316ebd6ffbcc3cd93996e5
960399552 <wuzihao4770@gmail.com>
2018-10-04 21:02:57 -0700

    final

--- src/database/Post.java
+++ src/database/Post.java
@@ -6,46 +6,48 @@ import java.util.Map;
 
 public class Post {
     // Use Java generics to avoid linear searching for users by id, makes it O(1) instead of O(n)
-    private  static Map<Integer, Post> postidDict = new HashMap<>();
-    private static ArrayList<Post> allPosts = new ArrayList<>();//make it static so that every time new posts are pushed to this list
-    private int userid;
-    private int postid;
-    private String data;
+    private static Map<Integer, Post> postidDict = new HashMap<>();
+    private static ArrayList<Post> allPosts = new ArrayList<>();
 
-    public Post(Post original) {
-        data = null;
-        postid = -1;
-        userid = -1;
+    private  String data;
+    private  int userid;
+    private  int postid;
+
+    public void setPostid(int postid){
+        this.postid = postid;
+        System.out.println(postid);
     }
+    public String getData(){
+        return data;
+    }
+
     public Post(){
         allPosts.add(this);
     }
-    public Post(String postContent, int postid, int userid){
-        this.data = postContent;
+
+    public Post(String data, int postid, int userid){
+        this.data = data;
         this.postid = postid;
         this.userid = userid;
         allPosts.add(this);
         postidDict.put(postid, this);
     }
+
+    public static Post getPost(int postid) {
+        return  postidDict.get(postid);
+    }
+
+    public void register() {
+        postidDict.put(postid, this);
+    }
+
     public static void loadAll(){
-        for(int i = 0 ; i < allPosts.size(); i++){
+        for(int i=0; i < allPosts.size(); i++){
             allPosts.get(i).register();
         }
     }
-    public void register(){
-        postidDict.put(postid, this);
-    }
-    public static Map getInstance() { //singleton
+
+    public static Map getInstance(){
         return postidDict;
     }
-
-    public String getData() {
-        return data;
-    }
-
-    //    public void setPostContent(int maxLength) {
-//        if (maxLength < this.data.length()) {
-//            this.data = this.data.substring(0, maxLength);
-//        }
-//    }
 }
--- src/database/User.java
+++ src/database/User.java
@@ -7,29 +7,48 @@ import java.util.Map;
 public class User {
     // Use Java generics to avoid linear searching for users by id, makes it O(1) instead of O(n)
     private  static Map<Integer, User> useridDict = new HashMap<>();
-    private static ArrayList<User> allUsers = new ArrayList<>();//make it static so that every time new users are pushed to this list
-    private int userid;
-    private String username;
+    private static ArrayList<User> allUsers = new ArrayList<>();
 
+    public void setUsername(String username) {
+        this.username = username;
+        System.out.println(username);
+    }
+
+    private  String username;
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+        System.out.println(userid);
+    }
+
+    private  int userid;
 
     public User(){
         allUsers.add(this);
     }
+
     public User(String username, int userid){
-        this.userid = userid;
         this.username = username;
+        this.userid = userid;
         allUsers.add(this);
         useridDict.put(userid, this);
     }
+
+    public static User getUser(int userid){
+        return useridDict.get(userid);
+    }
+
+    public void register(){
+        useridDict.put(userid, this);
+    }
+
     public static void loadAll(){
         for(int i = 0 ; i < allUsers.size(); i++){
             allUsers.get(i).register();
         }
     }
-    public void register(){
-        useridDict.put(userid, this);
-    }
-    public static Map getInstance() { //singleton
+
+    public static Map getInstance(){
         return useridDict;
     }
 }
